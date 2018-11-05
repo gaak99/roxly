ca85eaff5d6ddad401bcfab355979f21aa96d556
Glenn Barry <gaak99@gaak99.com>
2018-11-05 18:48:44 -0500

    prelim testing

--- roxly/tmp-dev/roxmemayme.py
+++ roxly/tmp-dev/roxmemayme.py
@@ -20,10 +20,17 @@ if __name__ == '__main__':
     init_ancdb=True
     #roxme.clone(dry_run, src, nrevs, init_ancdb)
 
+    ## status
+    filepath='testdir2/roxly-test0.org'
+    roxme.status(filepath)
+
+    ## metameta
+    key=None
+    roxme.getmm(key)
+    
     ## log
     oneline=True
     recent=5
-    filepath='testdir2/roxly-test0.org'
     roxme.log(oneline, recent, filepath)
 
     ## cat
@@ -35,5 +42,17 @@ if __name__ == '__main__':
     diff_cmd=None
     reva='HEADMINUS1'
     revb='HEAD'
-    roxme.diff(diff_cmd, reva, revb, filepath)
+    #roxme.diff(diff_cmd, reva, revb, filepath)
+
+    ## merge2
+    dry_run=False
+    emacsclient_path=None
+    merge_cmd=None
+    #roxme.merge2(dry_run, emacsclient_path, merge_cmd, reva, revb, filepath)
+    #roxme.merge(dry_run, merge_cmd, reva, revb, filepath)
     
+    ## push
+    dry_run=True
+    add=True
+    post_push_clone=False
+    roxme.push(dry_run, add, post_push_clone, filepath)
